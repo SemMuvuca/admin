5f03b658d5bedfa89ff592b392bcca1ba7555a1c
JamesUNT <thiagoaugusto.inc@gmail.com>
2021-09-19 19:57:48 -0300

    adicionando gif

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,40 +1,40 @@
+
 <template>
-  <q-page class="container" :style="{backgroundImage: 'linear-gradient( 135deg, #4B79A1, #283E51)'}">
-    <div class="overflow-auto">
-      <div class="row q-col-gutter-md q-px-md q-pt-md">
-        <div class="col-md-6 col-sm-12 col-xs-12">
-        <card-base>
-          <line-chart/>
-        </card-base>
-        </div>
-        <div class="col-md-6 col-sm-12 col-xs-12">
-          <card-base>
-            <bar-chart/>
-          </card-base>
-        </div>
+  <q-page :style="{backgroundImage: 'linear-gradient( 135deg, #4B79A1, #283E51)'}">
+    <div class="column justify-center window-height">
+      <div class="col-4 self-center text-center" >
+         <!-- <img
+          alt="Quasar logo"
+          src="/analysis.svg"
+          style="width: 300px"
+        > -->
+        <lottie-player
+          src="https://assets1.lottiefiles.com/packages/lf20_xrrp4bkp.json"
+          background="transparent"
+          speed="1"
+          loop
+          autoplay
+        ></lottie-player>
+      </div>
+      <div class="col-4">
+          <p class="text-h3 text-white text-weight-bold full-width text-center">
+           Sem Muvuca Admin
+          </p>
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, defineAsyncComponent } from 'vue'
-import CardBase from 'components/CardBase'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'PageIndex',
-  components: {
-    CardBase,
-    LineChart: defineAsyncComponent( () => import('components/charts/LineChart')),
-    BarChart: defineAsyncComponent(() => import('components/charts/BarChart'))
-  },
-
-  setup () {
-
-
-    return {
-
-    }
-  }
 })
 </script>
+
+<style scoped>
+.pink-custom {
+  color: #D705DC;
+}
+</style>
