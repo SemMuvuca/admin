b9ac8294820fd78f6285db1217f5251d51309628
JamesUNT <thiagoaugusto.inc@gmail.com>
2021-09-19 20:01:21 -0300

    Criando tela de Apresentacao

---
+++ src/pages/apresentacao/Apresentacao.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex flex-center bg-blue-7">
+    <div class="row justify-center q-gutter-y-lg">
+      <div class="col-12 q-pb-md">
+        <div class="text-h2 text-center text-yellow-8 text-weight-bold">
+          Sem<br/>Muvuca <span class="text-blue-2 text-weight-bold">Admin</span>
+        </div>
+      </div>
+      <!-- <div class="col-8 q-py-md">
+        <div class="text-h6 text-center text-white text-weight-bold">
+          Scaneie seus produtos, pague e leve para casa
+        </div>
+      </div> -->
+        <q-btn
+          class="col-7 bg-yellow-8 q-mt-xl"
+          :to="{name: 'Registro'}"
+        >
+          crie sua conta
+        </q-btn>
+        <q-btn
+          flat
+          text-color="white"
+          class="col-7 bg-blue-9"
+          :to="{name: 'Login'}"
+        >
+          já tenho uma conta
+        </q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Apresentacao',
+
+  setup () {
+
+    return {
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
